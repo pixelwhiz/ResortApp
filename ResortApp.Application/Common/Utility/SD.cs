eb83a1f67188e256e7c411cfc61026966a5f3342
pixelwhiz <daffa01futn@gmail.com>
2025-07-13 14:48:43 +0700

    fix typo

--- ResortApp.Application/Common/Utility/SD.cs
+++ ResortApp.Application/Common/Utility/SD.cs
@@ -2,6 +2,6 @@ namespace ResortApp.Application.Common.Utility;
 
 public static class SD
 {
-    public const string Role_Customer = "Custoemr";
+    public const string Role_Customer = "Customer";
     public const string Role_Admin = "Admin";
 }
