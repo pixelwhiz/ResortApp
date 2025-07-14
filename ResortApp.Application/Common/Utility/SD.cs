57561631f385bfde0d8468a23132fb25899e7c15
pixelwhiz <daffa01futn@gmail.com>
2025-07-14 18:29:20 +0700

    Add Booking Status

--- ResortApp.Application/Common/Utility/SD.cs
+++ ResortApp.Application/Common/Utility/SD.cs
@@ -4,4 +4,11 @@ public static class SD
 {
     public const string Role_Customer = "Customer";
     public const string Role_Admin = "Admin";
+
+    public const string StatusPending = "Pending";
+    public const string StatusApproved = "Approved";
+    public const string StatusCheckIn = "CheckIn";
+    public const string StatusCompleted = "Completed";
+    public const string StatusCancelled = "Cancelled";
+    public const string StatusRefunded = "Refunded";
 }
