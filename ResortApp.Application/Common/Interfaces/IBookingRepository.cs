560956ce7f99f3d6f837e008f5a2af56661ebee9
pixelwhiz <daffa01futn@gmail.com>
2025-07-16 20:40:32 +0700

    initial commit

--- ResortApp.Application/Common/Interfaces/IBookingRepository.cs
+++ ResortApp.Application/Common/Interfaces/IBookingRepository.cs
@@ -5,4 +5,6 @@ namespace ResortApp.Application.Common.Interfaces;
 public interface IBookingRepository : IRepository<Booking>
 {
     void Update(Booking entity);
+    void UpdateStatus(int bookingId, string bookingStatus);
+    void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId);
 }
--- ResortApp.Infrastructure/Migrations/Repository/BookingRepository.cs
+++ ResortApp.Infrastructure/Migrations/Repository/BookingRepository.cs
@@ -1,4 +1,5 @@
 using ResortApp.Application.Common.Interfaces;
+using ResortApp.Application.Common.Utility;
 using ResortApp.Domain.Entities;
 using ResortApp.Infrastructure.Data;
 
@@ -17,6 +18,43 @@ public class BookingRepository : Repository<Booking>, IBookingRepository
     public void Update(Booking entity)
     {
         _db.Bookings.Update(entity);
+    }
+
+    public void UpdateStatus(int bookingId, string bookingStatus)
+    {
+        var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+        if (bookingFromDb != null)
+        {
+            bookingFromDb.Status = bookingStatus;
+            if (bookingStatus == SD.StatusCheckedIn)
+            {
+                bookingFromDb.ActualCheckInDate = DateTime.Now;
+            }
+
+            if (bookingStatus == SD.StatusCompleted)
+            {
+                bookingFromDb.ActualCheckOutDate = DateTime.Now;
+            }
+        }
+    }
+
+    public void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId)
+    {
+        var bookingFromDb = _db.Bookings.FirstOrDefault(m => m.Id == bookingId);
+        if (bookingFromDb != null)
+        {
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                bookingFromDb.StripeSessionId = sessionId;
+            }
+
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                bookingFromDb.StripePaymentIntentId = sessionId;
+                bookingFromDb.PaymentDate = DateTime.Now;
+                bookingFromDb.IsPaymentSuccessful = true;
+            }
+        }
     }
 
 }
