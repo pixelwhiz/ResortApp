2055ef039fc130e4d1d5e5785040f3e0f4dbfd06
pixelwhiz <daffa01futn@gmail.com>
2025-07-23 23:00:03 +0700

    Styling Radial Chart

--- ResortApp.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
+++ ResortApp.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -34,40 +34,28 @@ function loadTotalBookingRadialChart() {
 function loadRadialBarChart(id, data) {
     var options = {
         chart: {
-            height: 280,
-            type: "radialBar"
+            height: 90,
+            width: 90,
+            type: "radialBar",
+            sparkLine: {
+                enabled: true
+            },
+            offsetY: -10,
         },
 
-        series: [67],
+        series: data.series,
 
         plotOptions: {
             radialBar: {
-                hollow: {
-                    margin: 15,
-                    size: "70%"
-                },
-
                 dataLabels: {
-                    showOn: "always",
-                    name: {
-                        offsetY: -10,
-                        show: true,
-                        color: "#888",
-                        fontSize: "13px"
-                    },
                     value: {
-                        color: "#111",
-                        fontSize: "30px",
-                        show: true
+                        offsetY: -10,
                     }
                 }
             }
         },
 
-        stroke: {
-            lineCap: "round",
-        },
-        labels: ["Progress"]
+        labels: [""]
     };
 
     var chart = new ApexCharts(document.querySelector("#"+id), options);
