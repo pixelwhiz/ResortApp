9deaa6d9b86989002cbd1e37a4fae3c232a67e59
pixelwhiz <daffa01futn@gmail.com>
2025-07-25 02:38:41 +0700

    LineChart 1

---
+++ ResortApp.Web/wwwroot/js/dashboard/getCustomerAndBookingLineChart.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    loadCustomerAndBookingLineChart();
+});
+
+function loadCustomerAndBookingLineChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetMemberAndBookingLineChartData",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            loadLineChart("newMembersAndBookingsLineChart", data);
+            $(".chart-spinner").hide();
+        }
+    });
+}
+
+function loadLineChart(id, data) {
+    var chartColors = getChartColorsArray(id);
+    options = {
+        colors: chartColors,
+        series: data.series,
+
+        chart: {
+            height: 350,
+            type: 'line',
+        },
+
+        stroke: {
+            show: false
+        },
+
+        markers: {
+            size: 0,
+            hover: {
+                sizeOffset: 6
+            }
+        },
+
+        xaxis: {
+            categories: data.categories,
+        },
+
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
