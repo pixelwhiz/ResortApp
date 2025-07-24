0435436446143981334e8b1acda207529991b000
pixelwhiz <daffa01futn@gmail.com>
2025-07-24 20:47:32 +0700

    Custom Color for Radial Chart

--- ResortApp.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
+++ ResortApp.Web/wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -30,34 +30,3 @@ function loadTotalBookingRadialChart() {
         }
     });
 }
-
-function loadRadialBarChart(id, data) {
-    var options = {
-        chart: {
-            height: 90,
-            width: 90,
-            type: "radialBar",
-            sparkLine: {
-                enabled: true
-            },
-            offsetY: -10,
-        },
-
-        series: data.series,
-
-        plotOptions: {
-            radialBar: {
-                dataLabels: {
-                    value: {
-                        offsetY: -10,
-                    }
-                }
-            }
-        },
-
-        labels: [""]
-    };
-    
-    var chart = new ApexCharts(document.querySelector("#"+id), options);
-    chart.render();
-}
