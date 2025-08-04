b4f1b89ac96a089e652e72d9c474526fb3b3ca61
pixelwhiz <daffa01futn@gmail.com>
2025-08-04 14:23:32 +0700

    Deploy to My Windows Hosting

---
+++ ResortApp.Infrastructure/Migrations/20250803183243_addnew2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResortApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnew2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
